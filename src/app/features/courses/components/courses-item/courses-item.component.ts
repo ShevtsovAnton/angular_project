7dfc6d8239ef30335ef10a7cc58d10ccbbd5f0a9
Shevtsov <a0660576041@gmail.com>
2019-11-26 05:47:47 +0200

    task-06 feat: add onPush change detection strategy

--- src/app/features/courses/components/courses-item/courses-item.component.ts
+++ src/app/features/courses/components/courses-item/courses-item.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { CoursesItemModel } from '../../models/courses-item.model';
 
 @Component({
   selector: 'app-courses-item',
   templateUrl: './courses-item.component.html',
-  styleUrls: ['./courses-item.component.scss']
+  styleUrls: ['./courses-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CoursesItemComponent {
 
