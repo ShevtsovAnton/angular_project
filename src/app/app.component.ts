4589a6165bef0737e734ef68488b5f0ad725d061
Shevtsov <a0660576041@gmail.com>
2019-12-14 08:37:21 +0200

    fix: remove unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { AuthorizationService } from './features/login/services/authorization.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
