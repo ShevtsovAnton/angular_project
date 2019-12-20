7dc0a5c4260793238d4665fecc4875a68278f29e
Shevtsov <a0660576041@gmail.com>
2019-12-20 17:00:26 +0200

    fix: route enum fix

--- src/app/shared/enums/routes.enum.ts
+++ src/app/shared/enums/routes.enum.ts
@@ -1,6 +1,6 @@
 export enum AppRoutes {
-    Login = '/login',
-    Courses = '/courses',
-    Courses_New = '/courses/new',
-    Courses_Id = '/courses/:id'
+    Login = 'login',
+    Courses = 'courses',
+    Courses_New = 'courses/new',
+    Courses_Id = 'courses/:id'
 }
